33daa59ed4de2c11bf32443070372bcc3159c5e4
Samuel Kraft <samuelkraft@me.com>
2021-01-24 21:28:43 +0100

    Add books to sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -2,16 +2,20 @@
 const fs = require('fs')
 const globby = require('globby')
 const prettier = require('prettier')
+const slugify = require('slugify')
 
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettier.config.js')
 
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby(['pages/*.tsx', 'data/**/*.mdx', '!pages/_*.tsx', '!pages/api'])
+  const bookRes = await fetch(`https://notion-api.splitbee.io/v1/table/b84d503315b24b7e8326ba6012dfddde`)
+  const bookData = await bookRes.json()
+  const books = bookData?.filter(book => book.Status == 'Published').map(book => `/books/${slugify(book.Name, { lower: true })}`)
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
+            ${[...pages, ...books]
               .map(page => {
                 const path = page.replace('pages', '').replace('data', '').replace('.tsx', '').replace('.mdx', '')
                 const route = path === '/index' ? '' : path
