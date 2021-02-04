fdf522736be9275a1dc78f6f213902fb04c93cf0
Samuel Kraft <samuelkraft@me.com>
2021-02-04 22:38:43 +0100

    Add blog post

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -7,7 +7,7 @@ import Button from 'components/button'
 
 import styles from './newsletter.module.scss'
 
-const Newsletter = () => {
+const Newsletter = ({ title }: { title?: string }) => {
   const { query } = useRouter() as NextRouter
   const inputEl = useRef(null)
   const [message, setMessage] = useState('')
@@ -57,7 +57,7 @@ const Newsletter = () => {
     <form onSubmit={subscribe} className={styles.wrapper}>
       <header className={styles.header}>
         <Send />
-        <h4 className={styles.title}>Subscribe to the newsletter</h4>
+        <h4 className={styles.title}>{title || 'Subscribe to the newsletter'}</h4>
       </header>
       <p className={styles.description}>You&apos;ll get an email when new content is posted. No spam, unsubscribe at any time.</p>
       <div className={cn(styles.inputWrapper, message && styles.hidden)}>
