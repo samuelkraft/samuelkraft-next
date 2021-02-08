cfe879c36e71565f324a1febbdc5e626790f629d
Samuel Kraft <samuelkraft@me.com>
2021-02-08 22:20:20 +0100

    Correct types

--- types.ts
+++ types.ts
@@ -1,7 +1,7 @@
 declare global {
   interface Window {
     goatcounter: {
-      count: (data: { path: string }) => void // eslint-disable-line
+      count: (data: { path: string; title?: string; referrer?: string; event?: boolean }) => void // eslint-disable-line
     }
   }
 }
