6995c5caa1cdb45440ede76cf95b378dba4b3d14
Samuel Kraft <samuelkraft@me.com>
2021-02-22 21:08:50 +0100

    New font (#20)
    * newfont
    
    * Fix theme toggle on safari

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,8 +23,9 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          <link rel="webmention" href="https://webmention.io/samuelkraft.com/webmention" />
-          <link rel="pingback" href="https://webmention.io/samuelkraft.com/xmlrpc" />
+          <link rel="preload" href="/fonts/GTWalsheimPro-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/GTWalsheimPro-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/GTWalsheimPro-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
