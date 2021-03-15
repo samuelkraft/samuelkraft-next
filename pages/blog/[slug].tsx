bbe236d2baaf7367b77cbd580b53fc67687cd0f9
Samuel Kraft <samuelkraft@me.com>
2021-03-15 09:12:07 +0100

    Add Parallax component

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -25,11 +25,10 @@ import BlogImage from 'components/blogimage'
 import SegmentedControl from 'components/segmentedcontrol'
 import Messages, { TailBreakdown } from 'components/messages'
 import AnimatedMessages from 'components/animatedmessages'
+import Parallax from 'components/parallax'
 
 // Utils
 import { postFilePaths, POSTS_PATH } from 'utils/mdxutils'
-import { formatDate } from 'lib/formatdate'
-
 import styles from './post.module.scss'
 
 const CustomLink = (props: { href: string }) => {
@@ -63,6 +62,7 @@ const components = {
   Messages,
   AnimatedMessages,
   TailBreakdown,
+  Parallax,
 }
 
 export type Meta = {
@@ -92,6 +92,18 @@ type PostProps = {
 const Post = ({ source }: PostProps): JSX.Element => {
   const content = hydrate(source, { components })
   const { scope: meta } = source
+  const formattedPublishDate = new Date(meta.publishedAt).toLocaleString('en-US', {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric',
+  })
+  const formattedUpdatedDate = meta.updatedAt
+    ? new Date(meta.updatedAt).toLocaleString('en-US', {
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+      })
+    : null
 
   const seoTitle = `${meta.title} | Samuel Kraft`
   const seoDesc = `${meta.summary}`
@@ -132,8 +144,8 @@ const Post = ({ source }: PostProps): JSX.Element => {
       {meta.image && <BlogImage src={meta.image} alt={meta.title} />}
       <PageHeader title={meta.title} compact>
         <p className={styles.meta}>
-          Published on <time dateTime={meta.publishedAt}>{formatDate(meta.publishedAt)}</time>
-          {meta.updatedAt ? ` (Updated ${formatDate(meta.updatedAt)})` : ''} <span>&middot;</span> {meta.readingTime.text}
+          Published on <time dateTime={meta.publishedAt}>{formattedPublishDate}</time>
+          {meta.updatedAt ? ` (Updated ${formattedUpdatedDate})` : ''} <span>&middot;</span> {meta.readingTime.text}
           <HitCounter slug={meta.slug} />
         </p>
       </PageHeader>
