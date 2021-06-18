4ca8947d2b10f951a642e3190876ca54be9880c5
Samuel Kraft <samuelkraft@me.com>
2021-06-18 09:42:18 +0200

    Testing

--- components/pagetransition.tsx
+++ components/pagetransition.tsx
@@ -5,23 +5,15 @@ const variants = {
   initial: {
     opacity: 0,
     y: 8,
-    transition: {
-      duration: 0.2,
-      ease: [0.61, 1, 0.88, 1],
-    },
   },
   enter: {
     opacity: 1,
     y: 0,
-    transition: {
-      duration: 0.4,
-      ease: [0.61, 1, 0.88, 1],
-    },
   },
 }
 
 const PageTransition = ({ children }: { children: ReactNode }): JSX.Element => (
-  <motion.div initial="initial" animate="enter" exit="initial" variants={variants}>
+  <motion.div initial="initial" animate="enter" exit="initial" variants={variants} transition={{ duration: 0.4, ease: [0.61, 1, 0.88, 1] }}>
     {children}
   </motion.div>
 )
