f51d9a8bf35c8d69470c8d519719113cacfe973d
Samuel Kraft <samuelkraft@me.com>
2021-06-18 09:37:26 +0200

    Revert

--- components/pagetransition.tsx
+++ components/pagetransition.tsx
@@ -1,10 +1,14 @@
 import { ReactNode } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const variants = {
   initial: {
     opacity: 0,
     y: 8,
+    transition: {
+      duration: 0.2,
+      ease: [0.61, 1, 0.88, 1],
+    },
   },
   enter: {
     opacity: 1,
@@ -17,11 +21,9 @@ const variants = {
 }
 
 const PageTransition = ({ children }: { children: ReactNode }): JSX.Element => (
-  <AnimatePresence initial={false}>
-    <motion.div initial="initial" animate="enter" variants={variants}>
-      {children}
-    </motion.div>
-  </AnimatePresence>
+  <motion.div initial="initial" animate="enter" exit="initial" variants={variants}>
+    {children}
+  </motion.div>
 )
 
 export default PageTransition
