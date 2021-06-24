ae4c51103611bfc7458a9e324b66191fdc9fbe85
Samuel Kraft <samuelkraft@me.com>
2021-06-24 07:17:24 +0200

    Combine likes with webmentions

--- pages/api/likes.ts
+++ pages/api/likes.ts
@@ -26,6 +26,11 @@ module.exports = async (req, res) => {
   // Fetch the document for-real
   const document = (await client.query(q.Get(q.Match(q.Index('likes_by_slug'), slug)))) as documentType
 
+  // Fetch webmentions
+  const webmentions = await fetch(`https://webmention.io/api/mentions?target=https://samuelkraft.com/blog/${slug}&per-page=10000`)
+  const mentions = await webmentions.json()
+  const numberOfmentions = mentions?.links?.length
+
   if (req.method === 'POST') {
     await client.query(
       q.Update(document.ref, {
@@ -35,11 +40,12 @@ module.exports = async (req, res) => {
       }),
     )
     const updatedDocument = (await client.query(q.Get(q.Match(q.Index('likes_by_slug'), slug)))) as documentType
+
     return res.status(200).json({
-      likes: updatedDocument.data.likes,
+      likes: numberOfmentions > 0 ? updatedDocument.data.likes + numberOfmentions : updatedDocument.data.likes,
     })
   }
   return res.status(200).json({
-    likes: document.data.likes,
+    likes: numberOfmentions > 0 ? document.data.likes + numberOfmentions : document.data.likes,
   })
 }
