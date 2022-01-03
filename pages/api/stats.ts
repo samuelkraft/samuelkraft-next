8cd10d75cf70c8b222f3c32d52230945a0786b58
Samuel Kraft <samuelkraft@me.com>
2022-01-03 21:58:28 +0100

    Update initial stats sorting

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -13,28 +13,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const posts = allPosts.map(post => pick(post, ['slug', 'title', 'publishedAt']))
 
   const postsWithLikes = await Promise.all(
-    posts.map(async post => {
-      // Fetch webmentions
-      const numberOfmentions = await getMentionsForSlug(post.slug)
+    posts
+      .sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
+      .map(async post => {
+        // Fetch webmentions
+        const numberOfmentions = await getMentionsForSlug(post.slug)
 
-      // Fetch fauna likes
-      type documentType = { ref: string; data: { likes: number } }
-      const likesDocument = (await client.query(q.Get(q.Match(q.Index('likes_by_slug'), post.slug)))) as documentType
-      const totalLikes = numberOfmentions > 0 ? likesDocument.data.likes + numberOfmentions : likesDocument.data.likes
+        // Fetch fauna likes
+        type documentType = { ref: string; data: { likes: number } }
+        const likesDocument = (await client.query(q.Get(q.Match(q.Index('likes_by_slug'), post.slug)))) as documentType
+        const totalLikes = numberOfmentions > 0 ? likesDocument.data.likes + numberOfmentions : likesDocument.data.likes
 
-      // Fetch fauna hits
-      const hitsDocument = (await client.query(q.Get(q.Match(q.Index('hits_by_slug'), post.slug)))) as {
-        ref: string
-        data: { hits: number }
-      }
+        // Fetch fauna hits
+        const hitsDocument = (await client.query(q.Get(q.Match(q.Index('hits_by_slug'), post.slug)))) as {
+          ref: string
+          data: { hits: number }
+        }
 
-      return {
-        ...post,
-        id: post.slug,
-        likes: totalLikes,
-        views: hitsDocument.data.hits,
-      }
-    }),
+        return {
+          ...post,
+          id: post.slug,
+          likes: totalLikes,
+          views: hitsDocument.data.hits,
+        }
+      }),
   )
 
   res.status(200).json({ posts: postsWithLikes })
