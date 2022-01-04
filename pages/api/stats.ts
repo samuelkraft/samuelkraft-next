63bd112f813a5f07cc719ba388945de53da14388
Samuel Kraft <samuelkraft@me.com>
2022-01-04 16:08:04 +0100

    Update stats.ts

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -11,7 +11,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   })
 
   const posts = allPosts.map(post => pick(post, ['slug', 'title', 'publishedAt', 'image', 'tags', 'summary']))
-
   const postsWithLikes = await Promise.all(
     posts
       .sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
