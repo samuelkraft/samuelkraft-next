a80bee950f7125691e5523ae327b7931aa049a38
Samuel Kraft <samuelkraft@me.com>
2022-06-06 09:46:08 +0200

    Update about.tsx

--- website/src/pages/about.tsx
+++ website/src/pages/about.tsx
@@ -1,8 +1,45 @@
 import type { NextPage } from "next";
-import { Box } from "design-system";
+import { Text, Spacer, Stack } from "design-system";
+import Container from "components/Container";
 
 const About: NextPage = () => {
-  return <Box>About</Box>;
+  return (
+    <>
+      <Spacer space={9} />
+      <Spacer space={9} />
+      <Container width="blog">
+        <Stack space={4} direction="column">
+          <Text>
+            Hey I’m Samuel, a frontend developer &amp; designer currently living
+            in 🇸🇪 Stockholm, Sweden.
+          </Text>
+          <Text>
+            Right now I’m working at{" "}
+            <a href="https://bitrefill.com/">Bitrefill</a>, designing &amp;
+            building the future of crypto. Before that I worked at{" "}
+            <a href="https://www.tracklib.com/">Tracklib</a>, the record store
+            for sampling.
+          </Text>
+          <Text>
+            I grew up in Nacka just outside of Stockholm (the perfect distance
+            from town while living next door to amazing nature 🏕) and come from
+            a background of studying Photography. I love working in the realm
+            between design and code. Some stuff that makes me excited are CSS,
+            React, Design Systems, Component Kits, UI Animation and delightful
+            interfaces ✨.
+          </Text>
+          <Text>
+            In my spare time I love being outdoors, training and travelling with
+            my family (check out{" "}
+            <a href="https://www.instagram.com/thejetlagfamily/">
+              @thejetlagfamily
+            </a>{" "}
+            ✈️).
+          </Text>
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default About;
