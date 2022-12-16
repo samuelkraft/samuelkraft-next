70c3629902e5f23adcbd5f3e899dfb6339d7b0ab
Samuel Kraft <samuelkraft@me.com>
2022-12-16 21:07:43 +0100

    Add "New" badge to blog (#86)

--- components/postlist.tsx
+++ components/postlist.tsx
@@ -11,13 +11,27 @@ export default function PostList({ posts }: PostListProps) {
   return (
     <ul className="flex flex-col gap-5 animated-list">
       {posts.length === 0 && <p>No posts found</p>}
-      {posts.map(({ publishedAt, slug, title }) => (
-        <li key={slug} className="transition-opacity">
-          <Section heading={formatDate(publishedAt)}>
-            <Link href={`/blog/${slug}`}>{title}</Link>
-          </Section>
-        </li>
-      ))}
+      {posts.map(({ publishedAt, slug, title }) => {
+        const publishDate = new Date(publishedAt);
+        const showNewBadge =
+          Math.abs(new Date(publishDate).getTime() - new Date().getTime()) /
+            (24 * 60 * 60 * 1000) <
+          30;
+        return (
+          <li key={slug} className="transition-opacity">
+            <Section heading={formatDate(publishedAt)}>
+              <Link href={`/blog/${slug}`}>
+                {title}
+                {showNewBadge && (
+                  <span className="inline-block px-1.5 py-[1px] relative -top-[2px] font-bold ml-2 text-[10px] uppercase rounded-full brand-gradient">
+                    New
+                  </span>
+                )}
+              </Link>
+            </Section>
+          </li>
+        );
+      })}
     </ul>
   );
 }
